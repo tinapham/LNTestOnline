89999742c2ef020acda426fdb1031fbdd1e1d3f5
shsl <letruonglamit@gmail.com>
2017-05-19 13:37:21 +0700

    add quiz-form.html

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+@author vetripandi
+@copyright http:www.vetbossel.in
+ */
+
+require_once 'messages.php';
+
+//site specific configuration declartion
+//define( 'BASE_PATH', 'http://localhost/php-quiz/');
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USERNAME', 'root');
+define( 'DB_PASSWORD', 'root');
+define( 'DB_NAME', 'quiz');
+
+function __autoload($class)
+{
+	$parts = explode('_', $class);
+	$path = implode(DIRECTORY_SEPARATOR,$parts);
+	require_once $path . '.php';
+}
